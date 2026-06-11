278649d77f4cadd6db4b67917c1f60e610d8914e
fkycia222 <fkycia@gmail.com>
2026-06-11 15:52:58 +0200

    Fix body

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { useState, useContext } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import DevBoardApp from "./DevBoardApp";
+import "./styles/Global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
